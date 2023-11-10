7819e5328a4bf90a84a316b0d8ab2fd46d1e150c
Shanehortonm <shanehortonm@gmail.com>
2023-11-10 06:41:10 -0500

    rework using tailwind and Flowbite

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
-import './App.css'
-import footer from './components/footer'
-import nav from './components/nav'
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
+//layouts
+import { RootLayout } from "./layouts/RootLayout";
+
+//pages
+import { Home } from "./pages/Home";
+import { Bio } from "./pages/Bio";
+import { Gallery } from "./pages/Gallery";
+import { Booking } from "./pages/Booking";
+import { NotFound } from "./pages/NotFound";
+
+//Router
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+
+      <Route path="Bio" element={<Bio />} />
+
+      <Route path="Gallery" element={<Gallery />} />
+
+      <Route path="Booking" element={<Booking />} />
+
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+);
 
 function App() {
-  return (
-    <>
-    <nav>{nav()}</nav>
-    <footer>{footer()}</footer>
-    </>
-    
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ src/layouts/RootLayout.tsx
@@ -0,0 +1,3 @@
+export const RootLayout = () => {
+  return <h1 className="text-blue-500"> Hello World </h1>;
+};
